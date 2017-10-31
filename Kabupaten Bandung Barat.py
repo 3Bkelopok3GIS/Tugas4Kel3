b4b3e9d4eaf4b55b5eb04d54ca7b70f807e74841
ajrinadarman <ajrinadarman20@gmail.com>
2017-11-01 00:37:47 +0800

    Create Kabupaten Bandung Barat.py

---
+++ Kabupaten Bandung Barat.py
@@ -0,0 +1,60 @@
+import folium
+
+m = folium.Map(
+    location=[-6.9045005,107.314102,11],
+    zoom_start=10,
+    )
+folium.Market(
+    Location=[-6.755192, 107.305862],
+    popup='Margaluyu',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.707458, 107.352554],
+    popup='Cipeundeuy',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.697911, 107.378647],
+    popup='Puteran',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.693819, 107.412979],
+    popup='Tenjolaut',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.745305, 107.511169],
+    popup='Ganjarsari',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.776671, 107.561294],
+    popup='Tugumukti',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.753488, 107.575027],
+    popup='Karyawangi',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.767807, 107.617599],
+    popup='Jayagiri',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.777353, 107.652618],
+    popup='Cikidang',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+folium.Market(
+    Location=[-6.794399, 107.677337],
+    popup='Wangunharja',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+
+
+    
